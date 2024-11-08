1fdd095edcbb056e893ace97d85c0416c553a8c0
henriwb <henriwb@hotmail.com>
2024-11-08 10:13:00 -0300

    Fix Object Spawner

--- 67BitsTestProj/Assets/Scripts/Gameplay/ObjectSpawner.cs
+++ 67BitsTestProj/Assets/Scripts/Gameplay/ObjectSpawner.cs
@@ -20,6 +20,7 @@ public class ObjectSpawner : MonoBehaviour
         {
             yield return new WaitForSeconds( interval);
             GameObject created = ObjectPoolManager.Instance.SpawnFromPool(poolTag, gameObject.transform.position, Quaternion.identity);
+            created.transform.localScale = Vector3.one;
             created.gameObject.SetActive(true);
             yield return new WaitForSeconds(1f);
         }
